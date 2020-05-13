ef969cef9d0b0c3972e5395151d9b97ae2b9d322
James Mummery <Jamie@Jamess-MacBook-Air.local>
2020-05-12 22:27:44 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,42 @@
+
+
+// I would code differently if it was an actual form
+// this code is just for presentation
+
+const myButton = document.querySelector(".arrow");
+const myInput = document.querySelector(".input");
+const errorIcon = document.querySelector(".error-icon");
+const errorMessage = document.querySelector(".error-message");
+
+// eventlistner for input not nessesary but I like the style
+
+myInput.addEventListener("input", function(event){
+  if (!myInput.validity.typeMismatch) {
+    errorMessage.style.visibility = "hidden";
+    errorIcon.style.visibility = "hidden";
+    myInput.setCustomValidity(" ");
+  }
+})
+
+// eventlistener for submit button
+
+myButton.onclick = function() {
+  if (myInput.validity.typeMismatch) {
+    errorMessage.textContent="Please provide a valid email";
+    errorMessage.style.visibility = "visible";
+    errorIcon.style.visibility = "visible";
+    myInput.setCustomValidity(" ");
+  } else {
+    errorMessage.textContent="Success";
+    errorMessage.style.visibility = "visible";
+    errorIcon.style.visibility = "hidden";
+    myInput.setCustomValidity(" ");
+  }
+}
+
+// hide error message when focus is away
+
+myInput.onblur = function() {
+  errorMessage.style.visibility = "hidden";
+  errorIcon.style.visibility = "hidden";
+}
