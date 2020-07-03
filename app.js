16711f41b98c7947cb63a38d7e5c8d9f96d85aa8
James Mummery <Jamie@Jamess-MacBook-Air.local>
2020-07-03 12:24:30 -0700

    fix footer

--- app.js
+++ app.js
@@ -1,42 +1,26 @@
-
-
-// I would code differently if it was an actual form
-// this code is just for presentation
-
 const myButton = document.querySelector(".arrow");
 const myInput = document.querySelector(".input");
 const errorIcon = document.querySelector(".error-icon");
 const errorMessage = document.querySelector(".error-message");
 
-// eventlistner for input not nessesary but I like the style
+const emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
-myInput.addEventListener("input", function(event){
-  if (!myInput.validity.typeMismatch) {
-    errorMessage.style.visibility = "hidden";
-    errorIcon.style.visibility = "hidden";
-    myInput.setCustomValidity(" ");
-  }
-})
 
-// eventlistener for submit button
-
-myButton.onclick = function() {
-  if (myInput.validity.typeMismatch) {
+myButton.onclick = function(event){
+  if(!myInput.value.match(emailFormat)){
     errorMessage.textContent="Please provide a valid email";
     errorMessage.style.visibility = "visible";
     errorIcon.style.visibility = "visible";
-    myInput.setCustomValidity(" ");
+      event.preventDefault();
   } else {
     errorMessage.textContent="";
-    errorMessage.style.visibility = "visible";
+    errorMessage.style.visibility = "hidden";
     errorIcon.style.visibility = "hidden";
-    myInput.setCustomValidity(" ");
+  };
+
+  myInput.onblur = function() {
+    errorMessage.style.visibility = "hidden";
+    errorIcon.style.visibility = "hidden";
   }
-}
 
-// hide error message when focus is away
-
-myInput.onblur = function() {
-  errorMessage.style.visibility = "hidden";
-  errorIcon.style.visibility = "hidden";
 }
